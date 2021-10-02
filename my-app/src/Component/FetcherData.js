08c0465fa2f02bf5ce7af6c3c3d4df4765d2d2ce
Antonin <antonin.boinega@acpi.tech>
2021-10-02 23:12:19 +0200

    Add feature 2

---
+++ my-app/src/Component/FetcherData.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import React from 'react'
+
+class FetcherData extends React.Component {
+	render() {
+		return(<div/>); 
+	}
+	
+	fetchtoptencrypto = () => {
+	console.log("hello")
+	}
+}
+
+
+
+export default FetcherData;
